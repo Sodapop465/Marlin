add2d082f4bc24a6d3b02442525f0187a84d5b1d
Kadin Le <kadinle465@gmail.com>
2026-01-20 22:47:18 -0700

    Changed smooth time back to usuable value, things seem to work now.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1163,7 +1163,7 @@
     //#define FTM_HOME_AND_PROBE                // Use FT Motion for homing / probing. Disable if FT Motion breaks these functions.
   #endif
 
-  #define FTM_LIN_ADV_SMOOTH_TIME        0.08f   // (s) Smoothing time for linear advance. Zero means no smoothing.
+  #define FTM_LIN_ADV_SMOOTH_TIME        0.04f   // (s) Smoothing time for linear advance. Zero means no smoothing.
 
   //#define FTM_DYNAMIC_FREQ                    // Enable for linear adjustment of XY shaping frequency according to Z or E
   #if ENABLED(FTM_DYNAMIC_FREQ)
